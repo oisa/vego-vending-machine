a8fc9ef5ecc8f233e8905fab020a22491b2c41d7
oisa <imucup@gmail.com>
2021-03-30 17:05:10 +1100

    Adds price options and display of total. Function to come to these components.

--- src/App.js
+++ src/App.js
@@ -13,18 +13,30 @@ function App() {
 
       <div className="vending-machine">
 
-        <div className="signage">
+        <section className="signage">
           <img src={logo} />
           <p>Vending Machine</p>
-        </div>
+        </section>
 
-        <div className="total">
+        <section className="money-container">
+          <div className="money-input">
+            <p>Enter how much you have:</p>
+            <div className="options-container">
+              <a className="option" value="0.10"href="#">10c</a>
+              <a className="option" value="0.20" href="#">20c</a>
+              <a className="option" value="0.50" href="#">50c</a>
+              <a className="option" value="1" href="#">$1</a>
+              <a className="option" value="2" href="#">$2</a>
+            </div>
+          </div>
 
-        </div>
-
-        <div className="money-input">
-
-        </div>
+          <div className="total">
+            <p>Total credit:</p>
+            <div className="total-display">
+              $0.20
+            </div>
+          </div>
+        </section>
 
       </div>
 
