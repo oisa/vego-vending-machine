4776977bb111b69ca30ff7e4e53815d35be77263
oisa <imucup@gmail.com>
2021-03-30 16:19:29 +1100

    Adds base styling for vending machine.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import logo from './assets/logo.svg';
 import './App.css';
 
+// Components
+
+
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-container">
+
+      <div className="vending-machine">
+
+        <div className="signage">
+          <img src={logo} />
+          <p>Vending Machine</p>
+        </div>
+
+        <div className="total">
+
+        </div>
+
+        <div className="money-input">
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
